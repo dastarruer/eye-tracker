2c73041eeeda9067cba7d24ca4a952b81d985fec
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-11-03 13:12:46 -0400

    Add EyeTracker class

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-if __name__ == "__main__":
-    print("hello world")
+class EyeTracker:
+    def eye_position(self) -> tuple[int, int]:
+        return (0, 0)
 
+
+if __name__ == "__main__":
+    eye_tracker = EyeTracker()
+    print(eye_tracker.eye_position())
