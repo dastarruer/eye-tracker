899da39bbcb93cd33ef040a8a0db94adb03d3eff
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-11-03 14:15:10 -0400

    Implement eye_position function for EyeTracker

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
+from eyeGestures.utils import VideoCapture
+from eyeGestures import EyeGestures_v2
+
+
 class EyeTracker:
+    def __init__(self):
+        # Initialize gesture engine and video capture
+        self._gestures = EyeGestures_v2()
+        self._cap = VideoCapture(0)
+        self._calibrate = True
+        self._screen_width = 1920
+        self._screen_height = 1080
+
     def eye_position(self) -> tuple[int, int]:
-        return (0, 0)
+        """
+        Return the eye position as (x, y) coordinates.
+        """
+
+        ret, frame = self._cap.read()
+        # TODO: This function keeps throwing an error whenever it cannot detect a face/eyes
+        event, cevent = self._gestures.step(
+            frame, self._calibrate, self._screen_width, self._screen_height, context="my_context"
+        )
+
+        if event:
+            return(event.point[0], event.point[1])
 
 
 if __name__ == "__main__":
     eye_tracker = EyeTracker()
-    print(eye_tracker.eye_position())
+
+    while True:
+        print(eye_tracker.eye_position())
+
+
